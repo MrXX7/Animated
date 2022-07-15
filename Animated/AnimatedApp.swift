59db001b7612634a48a8222504b8845e42b7e60f
Oncu <oohancan@gmail.com>
2022-07-15 21:46:48 +0300

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Oncu Can on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
