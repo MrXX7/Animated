6c977c113271d723c83daafdace6e04d5ec66a88
Oncu <oohancan@gmail.com>
2022-07-18 14:36:21 +0300

    add CustomFont, and button and text for OnboardingView

---
+++ Animated/CustomFont.swift
@@ -0,0 +1,20 @@
+//
+//  CustomFont.swift
+//  Animated
+//
+//  Created by Oncu Can on 18.07.2022.
+//
+
+import SwiftUI
+
+struct CustomFont: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomFont_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomFont()
+    }
+}
--- Animated/OnboardingView.swift
+++ Animated/OnboardingView.swift
@@ -12,22 +12,45 @@ struct OnboardingView: View {
     let button = RiveViewModel(fileName: "button")
     var body: some View {
         ZStack {
-            RiveViewModel(fileName: "shapes").view()
-                .ignoresSafeArea()
-                .blur(radius: 30)
-                .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x: 200, y: 100)
-            )
-            button.view()
-                .frame(width: 236, height: 64)
-                .overlay(
-                    Label("Start the course", systemImage: "arrow.forward")
-                        .offset(x: 4, y: 4)
-                        .font(.headline)
-                )
+            background
+            VStack {
+                Text("Learn design & code")
+                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                    .frame(width: 260, alignment: .leading)
+                Text("Don't skip design. Learnd design and code, by building real apps witch React and Swift. Complete courses about the best tools.")
+                    .font(.headline)
+                
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Start the course", systemImage: "arrow.forward")
+                            .offset(x: 4, y: 4)
+                            .font(.headline)
+                    )
+                    .background(
+                        Color.black
+                            .cornerRadius(30)
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y: 10)
+                    
+                    
+                    )
+                    .onTapGesture {
+                        try? button.play(animationName: "active")
+                }
+            }
         }
+    }
+    var background: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+            Image("Spline")
+                .blur(radius: 50)
+                .offset(x: 200, y: 100)
+        )
     }
 }
 
