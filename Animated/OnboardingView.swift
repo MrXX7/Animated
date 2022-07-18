04b9f74bed86b743baa62943093e0cff780864e5
Oncu <oohancan@gmail.com>
2022-07-18 16:14:41 +0300

    add SignInView

--- Animated/OnboardingView.swift
+++ Animated/OnboardingView.swift
@@ -13,13 +13,16 @@ struct OnboardingView: View {
     var body: some View {
         ZStack {
             background
-            VStack {
+            VStack(alignment: .leading, spacing: 16) {
                 Text("Learn design & code")
                     .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
                     .frame(width: 260, alignment: .leading)
                 Text("Don't skip design. Learnd design and code, by building real apps witch React and Swift. Complete courses about the best tools.")
-                    .customFont(textStyle: .title)
+                    .customFont(.body)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                     
+                Spacer()
                 
                 button.view()
                     .frame(width: 236, height: 64)
@@ -40,7 +43,12 @@ struct OnboardingView: View {
                     .onTapGesture {
                         try? button.play(animationName: "active")
                 }
+                Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos source files and certificates")
+                    .customFont(.footnote)
+                    .opacity(0.7)
             }
+            .padding(40)
+            .padding(.top, 40)
         }
     }
     var background: some View {
