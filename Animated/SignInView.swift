7b26726edf4c7dbe7c565653f64062cf1193cbec
Oncu <oohancan@gmail.com>
2022-07-18 16:24:05 +0300

    add text and logo for SignInView

--- Animated/SignInView.swift
+++ Animated/SignInView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct SignInView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 24) {
+            Text("Sign In")
+                .customFont(.largeTitle)
+            Text("Access to 240+ hours of content. Learn design and code, by building real apps with React and Swift.")
+                .customFont(.headline)
+            Divider()
+            
+            Text("Sign Up with Email, Apple or Google")
+                .customFont(.subheadline)
+                .foregroundColor(.secondary)
+            
+            HStack {
+                Image("Logo Email")
+                Spacer()
+                Image("Logo Apple")
+                Spacer()
+                Image("Logo Google")
+            }
+        }
+        .padding(30)
+        .background(.regularMaterial)
+        .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 5, x: 0, y: 3)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 30, x: 0, y: 30)
+        .padding()
     }
 }
 
