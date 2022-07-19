3600c4f8a84a19c57db1cde03bcbac9e26b16d15
Oncu <oohancan@gmail.com>
2022-07-19 20:25:42 +0300

    add text and overlay

--- Animated/SignInView.swift
+++ Animated/SignInView.swift
@@ -14,6 +14,12 @@ struct SignInView: View {
                 .customFont(.largeTitle)
             Text("Access to 240+ hours of content. Learn design and code, by building real apps with React and Swift.")
                 .customFont(.headline)
+            VStack {
+                Text("Email")
+                    .customFont(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
             Divider()
             
             Text("Sign Up with Email, Apple or Google")
@@ -33,6 +39,13 @@ struct SignInView: View {
         .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 5, x: 0, y: 3)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 30, x: 0, y: 30)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .stroke(.linearGradient(colors:
+                    [.white.opacity(0.8), .white.opacity(0.1)],
+                    startPoint: .topLeading, endPoint:
+                    .bottomTrailing))
+            )
         .padding()
     }
 }
